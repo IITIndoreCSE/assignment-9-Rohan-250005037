73db40fa4d10004a442bc8788426c3d8ecbb0ab1
Rohan-250005037 <mems250005037@iiti.ac.in>
2025-10-17 17:19:04 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Point {
+    float x;
+    float y;
+};
+
+int main() {
+    Point coor[6]; 
+
+
+    for (int i = 0; i < 6; i++) {  
+        float x, y;
+        cout << "Enter coordinates for point " << i + 1 << ":" << endl;
+        
+        cout << "x: ";
+        cin >> x;
+
+        cout << "y: ";
+        cin >> y;
+
+        coor[i] = {x, y};
+    }
+
+
+    int count = 0;
+    for (int i = 0; i < 6; i++) {  
+        if (coor[i].x >= 0 && coor[i].y >= 0) { 
+            count++;
+        }
+    }
+
+    cout << "Number points in first quadrant: " << count << endl;
+
+    return 0;
+}
 
